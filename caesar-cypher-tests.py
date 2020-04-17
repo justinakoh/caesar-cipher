166d0692e248ed04cc21f4a203ec3cb569de296f
justinakoh <justina.y.koh@gmail.com>
2020-04-17 16:43:45 +1200

    Start code from scratch for TDD

--- caesar-cypher-tests.py
+++
@@ -1,3 +0,0 @@
-# These are the test cases for the caesar-cypher
-from caesar_cypher import main
-from unittest import mock
--- test_caesar_cipher.py
+++ test_caesar_cipher.py
@@ -1,3 +1,8 @@
 # These are the test cases for the caesar-cypher
+import unittest
 from caesar_cypher import main
-from unittest import mock
+
+class TestCipher(unittest.TestCase):
+
+    def test_invalid_user_input(self):
+        
