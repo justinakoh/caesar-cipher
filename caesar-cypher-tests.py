79de9b78f793f39f56f44ac5e79a722cca8f21b2
justinakoh <justina.y.koh@gmail.com>
2020-04-17 16:33:32 +1200

    Add in test file

--- caesar-cypher-tests.py
+++ caesar-cypher-tests.py
@@ -1,11 +1,3 @@
 # These are the test cases for the caesar-cypher
-from caesar_cypher import Caesar_cypher
-import unittest
-from unitest import mock
-from unittet import TestCase
-
-
-@mock.patch("")
-def test_it_correctly_outputs_short_string():
-    with mock.patch("builtins.input", first_input="1"):
-        assert caesar_cypher() == "Please enter a letter!"
+from caesar_cypher import main
+from unittest import mock
