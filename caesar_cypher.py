11c117f212203319a7b9564c308ccf1823636696
justinakoh <justina.y.koh@gmail.com>
2020-04-18 22:16:23 +1200

    Add in test cases for TDD

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,6 +1,23 @@
 # This is a script to translate into and out of the caesar cypher. This is a simple subsitution cypher which involves substituting each letter in the message you want to be encoded, by substituting each letter by a specific number across
 def main():
-    print("This is a caesar cipher encoder/decoder")
+    enter_alg = input(
+        "This is a caesar cipher encoder/decoder. Do you wish to continue? Y/n"
+    )
+    if enter_alg == "" or enter_alg == "y" or enter_alg == "Y":
+        known_letter = input("Please enter a letter you'd like to replace")
+        replacement_letter = input(
+            "Now please enter the letter you'd like to replace it with"
+        )
+        calc_spaces_to_shift_by(known_letter, replacement_letter)
+    else:
+        return "Exiting the program now"
+        exit()
+
+
+# This calculates the spaces to shift the alphabet by
+def calc_spaces_to_shift_by(known_letter, replacement_letter):
+    spaces_to_shift = abs(ord(known_letter) - ord(replacement_letter))
+    return spaces_to_shift
 
 
 if __name__ == "__main__":
--- test_caesar_cipher.py
+++ test_caesar_cipher.py
@@ -1,8 +1,22 @@
 # These are the test cases for the caesar-cypher
 import unittest
+import mock
+from mock import patch
 from caesar_cypher import main
+from caesar_cypher import calc_spaces_to_shift_by
 
-class TestCipher(unittest.TestCase):
 
-    def test_invalid_user_input(self):
-        
+class test_spaces_to_shift_by(unittest.TestCase):
+    def test_it_can_calculate_normal_difference(self):
+        expected = 1
+        actual = calc_spaces_to_shift_by("a", "b")
+        self.assertEqual(actual, expected)
+
+    def test_it_returns_positive_numbers(self):
+        expected = 25
+        actual = calc_spaces_to_shift_by("b", "a")
+        self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
